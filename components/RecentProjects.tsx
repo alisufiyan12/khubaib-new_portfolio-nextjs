7cebc708cb0de2a7b12a3d3034f1e8bcb7f4115f
Li-Grey <khubaib7236@gmail.com~>
2024-08-10 04:28:31 +0500

    sometest changes made

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,7 +13,7 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div  className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
           <div
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
